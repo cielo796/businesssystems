eecb5a1bc8ad8c634e8dcdfd8d564b2ec294d239
cielo796 <serena796@gmail.com>
2022-07-27 06:23:12 +0900

    pagenation+

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -5,15 +5,17 @@ import {Button, ButtonToolbar,ButtonGroup} from 'react-bootstrap';
 import "./pagination.scss";
 
 export const Pagination = ({ totalCount }) => {
-  const PER_PAGE = 5
-
+  //ページコンテンツ
+  const PER_PAGE = 15
   const range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i)
 
   return (
     <div class="listnumber">
-      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
-          <Link to={`/information/${number}`}>{number}</Link>
-      ))}
+       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => ( 
+             <Link to={`/information/${number}`}>{number}</Link> 
+             
+             )) 
+       } 
     </div>
   )
 }
--- src/components/paginationnews.js
+++ src/components/paginationnews.js
@@ -1,19 +1,23 @@
 //components/pagination.js
 import React from "react"
 import { Link } from "gatsby"
-import {Button, ButtonToolbar,ButtonGroup} from 'react-bootstrap';
+import {Pagination} from 'react-bootstrap';
 import "./pagination.scss";
 
 export const PaginationNews = ({ totalCount }) => {
-  const PER_PAGE = 5
+  const PER_PAGE = 15
 
   const range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i)
 
   return (
     <div class="listnumber">
-      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
+      {
+
+        range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
           <Link to={`/informationnews/${number}`}>{number}</Link>
-      ))}
+      ))
+      
+      }
     </div>
   )
 }
