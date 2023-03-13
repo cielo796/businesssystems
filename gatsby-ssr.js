058d41bb252d3d4ef06615596fa9e798e5cf327a
cielo796 <serena796@gmail.com>
2023-03-13 23:45:21 +0900

    forms

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,15 @@
  */
 
 // You can delete this file if you're not using it
-exports.onPreRenderHTML = function onPreRenderHTML({
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script key="zoho-forms-script" type="text/javascript" src="https://forms.zohopublic.com/js/zf_v1/iframe.js"></script>
+  ]);
+};
+
+export const onPreRenderHTML = function onPreRenderHTML({
     getHeadComponents,
     replaceHeadComponents,
   }) {
@@ -26,3 +34,34 @@ exports.onPreRenderHTML = function onPreRenderHTML({
   
     replaceHeadComponents(headComponents);
   };
+
+// import React from 'react';
+
+// export const onRenderBody = ({ setHeadComponents }) => {
+//   setHeadComponents([
+//     <script key="zoho-forms-script" type="text/javascript" src="https://forms.zohopublic.com/js/zf_v1/iframe.js"></script>
+//   ]);
+// };
+
+
+// exports.onPreRenderHTML = function onPreRenderHTML({
+//     getHeadComponents,
+//     replaceHeadComponents,
+//   }) {
+//     const headComponents = getHeadComponents();
+//     headComponents.sort((a, b) => {
+//       if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+//         return 0;
+//       }
+  
+//       if (a.type === 'style') {
+//         return 1;
+//       } else if (b.type === 'style') {
+//         return -1;
+//       }
+  
+//       return 0;
+//     });
+  
+//     replaceHeadComponents(headComponents);
+//   };
